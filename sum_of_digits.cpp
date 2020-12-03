b71fcefa6fef9d25a7fd662deb200a204f44d80d
Priyanshu <72227659+2020Priyanshu@users.noreply.github.com>
2020-12-03 16:13:56 +0530

    sum_of_digits.cpp

--- sum_of_digits.cpp
+++ sum_of_digits.cpp
@@ -5,9 +5,9 @@ int main()
 	int sum = 0,num;
 	std::cout << "Enter the number : ";
 	std::cin >> num;
-	for (int i = 0; num % 10 != 0; num = num / 10)
+	for (int i = 0; num % 10 != 0; num = num / 10)		//in step up and step down num is kept changing as each time the last digit is eliminated
 	{
-		sum += num % 10;
+		sum += num % 10;				//num%10 gives the last digit of the number num
 	}
 	std::cout << sum;
 }
