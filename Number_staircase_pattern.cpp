1c4a3302093e94854be443678d5e9625ff577b60
Priyanshu <72227659+2020Priyanshu@users.noreply.github.com>
2020-12-03 16:04:40 +0530

    Number_staircase_pattern.cpp

--- Number_staircase_pattern.cpp
+++ Number_staircase_pattern.cpp
@@ -7,14 +7,14 @@ int main()
 {
 	int row,val;
 	std::cout << "Enter the row size : ";
-	std::cin >> row;
-	for (int i = 1; i <= row; i++)
+	std::cin >> row;			//Inputs Row
+	for (int i = 1; i <= row; i++)		//Iterates through the row
 	{
-		val = i;
-		for (int j = 1; j <= (2 * i - 1); j++)
+		val = i;			//Initialises the value of val to the current row
+		for (int j = 1; j <= (2 * i - 1); j++)		//Iterates through the columns - the end limit is 2*i-1 as for ith row there are 2*i-1 columns
 		{
 			std::cout <<'\t'<< val;
-			if (j >= i)
+			if (j >= i)				//Checks for the iterations beyond middle column
 			{
 				val--;
 			}
